72395cc8b65b6f5372affbe7a0febd3c1b4fd14c
starchikov_in <starchikov_in@drom.ru>
2022-05-15 20:36:19 +1000

    [DZ-5.4]

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1geu0049le0d2th0pru"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gb30s6gr3fqd124hdv"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8vdidmktqos56nb991"
+}
