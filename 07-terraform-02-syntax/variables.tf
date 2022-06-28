93042868f4055aeb97185351ddc5a64094837ecb
starchikov_in <starchikov_in@drom.ru>
2022-06-28 22:34:34 +1000

    DZ-7.2

---
+++ 07-terraform-02-syntax/variables.tf
@@ -0,0 +1,13 @@
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "aaaaaaaaaaaaaaaaaaaa"
+}
+
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "bbbbbbbbbbbbbbbbbbb"
+}
+
+variable "centos-7-base" {
+  default = "ccccccccccccccccccc"
+}
